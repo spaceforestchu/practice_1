da15ecaa3d90d28bc7faf888eaded56f9359c652
SpaceForestChu <spaceforestchu@gmail.com>
2016-10-05 10:47:56 -0400

    condensed sub modules

--- public/src/components/Main.js
+++ public/src/components/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import Home from './layout/Home'
-
+// import Home from './layout/Home'
+// import Search from './layout/Search'
+import {Home, Search} from './layout'
 
 class Main extends Component {
   render() {
     return (
       <div>
-          <Home />
+          <Search />
       </div>
     )
   }
