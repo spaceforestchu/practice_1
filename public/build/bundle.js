78ef37e6551c375648b87f4c853e26d09854b48e
SpaceForestChu <spaceforestchu@gmail.com>
2016-10-05 12:26:53 -0400

    got search params working

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -23950,6 +23950,10 @@
 	
 	var _react2 = _interopRequireDefault(_react);
 	
+	var _APIClient = __webpack_require__(181);
+	
+	var _APIClient2 = _interopRequireDefault(_APIClient);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -23971,6 +23975,15 @@
 	    key: 'componentWillMount',
 	    value: function componentWillMount() {
 	      console.log('Params = ' + JSON.stringify(this.props.params));
+	
+	      _APIClient2.default.get('/api/service', this.props.params, function (err, response) {
+	        if (err) {
+	          alert(err);
+	          return;
+	        }
+	
+	        console.log(JSON.stringify(response));
+	      });
 	    }
 	  }, {
 	    key: 'render',
--- public/src/components/layout/Search.js
+++ public/src/components/layout/Search.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import APIClient from '../../utils/APIClient'
 
 class Search extends Component {
 
   componentWillMount() {
     console.log('Params = ' + JSON.stringify(this.props.params))
+
+    APIClient.get('/api/service', this.props.params, (err, response) => {
+      if (err) {
+        alert(err)
+        return
+      }
+
+      console.log(JSON.stringify(response))
+    })
+
   }
 
   render(){
