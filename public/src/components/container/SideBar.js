62a00dc24f68cb4997630e5158a4bc019f6608e3
SpaceForestChu <spaceforestchu@gmail.com>
2016-10-05 13:04:23 -0400

    sidebar

---
+++ public/src/components/container/SideBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component {
+  render () {
+    return (
+      <header id="header" classNamme="no-sticky">
+  			<div id="header-wrap">
+  				<div class="container clearfix">
+
+  					<div id="primary-menu-trigger"><i classNamme="icon-reorder"></i></div>
+
+  					<!-- Logo
+  					============================================= -->
+  					<div id="logo" class="nobottomborder">
+  						<a href="index.html" class="standard-logo" data-dark-logo="images/logo-side-dark.png"><img src="images/logo-side.png" alt="Canvas Logo" /></a>
+  						<a href="index.html" class="retina-logo" data-dark-logo="images/logo-side-dark@2x.png"><img src="images/logo-side@2x.png" alt="Canvas Logo" /></a>
+  					</div>
+
+  					<nav id="primary-menu">
+
+  						<ul>
+  							<li><a href="index.html"><div>Home</div></a></li>
+  						</ul>
+
+  					</nav>
+
+
+  				</div>
+
+  			</div>
+
+  		</header>
+    )
+  }
+}
+
+export default Sidebar
