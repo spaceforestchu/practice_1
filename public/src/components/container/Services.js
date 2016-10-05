d1dfeba0e0774c499a5e70e3f5af73056e762e5c
SpaceForestChu <spaceforestchu@gmail.com>
2016-10-05 13:33:14 -0400

    cleared service.js

--- public/src/components/container/Services.js
+++ public/src/components/container/Services.js
@@ -7,71 +7,7 @@ class Services extends Component {
 
         <div className="content-wrap">
 
-      
-
           <div className="container clearfix">
-
-            <div className="col_one_third">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-screen i-alt"></i></a>
-                </div>
-                <h3>Responsive Layout</h3>
-                <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
-              </div>
-            </div>
-
-            <div className="col_one_third">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-eye i-alt"></i></a>
-                </div>
-                <h3>Retina Ready Graphics</h3>
-                <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
-              </div>
-            </div>
-
-            <div className="col_one_third col_last">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-beaker i-alt"></i></a>
-                </div>
-                <h3>Powerful Performance</h3>
-                <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
-              </div>
-            </div>
-
-            <div className="clear"></div>
-
-            <div className="col_one_third nobottommargin">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-stack i-alt"></i></a>
-                </div>
-                <h3>Premium Sliders Included</h3>
-                <p>Canvas included 20+ custom designed Slider Pages with Premium Sliders like Layer, Revolution, Swiper &amp; others.</p>
-              </div>
-            </div>
-
-            <div className="col_one_third nobottommargin">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-tint i-alt"></i></a>
-                </div>
-                <h3>Unlimited Color Options</h3>
-                <p>Change the color scheme of the Theme in a flash just by changing the 6-digit HEX code in the colors.php file.</p>
-              </div>
-            </div>
-
-            <div className="col_one_third nobottommargin col_last">
-              <div className="feature-box fbox-effect">
-                <div className="fbox-icon">
-                  <a href="#"><i className="icon-text-width i-alt"></i></a>
-                </div>
-                <h3>CUSTOMIZABLE FONTS</h3>
-                <p>Use any Font you like from Google Web Fonts, Typekit or other Web Fonts. They will blend in perfectly.</p>
-              </div>
-            </div>
 
 
           </div>
